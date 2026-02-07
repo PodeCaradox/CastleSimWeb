65c64a683d16d2126f20962313d52c64a1fcb70d
PodeCaradox <gamer3911@web.de>
2026-02-07 13:59:29 +0100

    update versions

--- src/assets/wasm/castle_sim_bg.wasm.d.ts
+++ src/assets/wasm/castle_sim_bg.wasm.d.ts
@@ -2,12 +2,12 @@
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
 export const init_game: (a: number, b: number) => void;
-export const wasm_bindgen__closure__destroy__h5889c74a83b49cd9: (a: number, b: number) => void;
+export const wasm_bindgen__closure__destroy__h269b17735e5fc350: (a: number, b: number) => void;
 export const wasm_bindgen__closure__destroy__hd9e334d357d3bb07: (a: number, b: number) => void;
-export const wasm_bindgen__convert__closures_____invoke__h184d73d7bfa6f575: (a: number, b: number, c: any, d: any) => void;
-export const wasm_bindgen__convert__closures_____invoke__h3451b5a12361e86f: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h6dd67367705a18ed: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h1032700cb4796663: (a: number, b: number, c: any) => void;
 export const wasm_bindgen__convert__closures_____invoke__h3ac7ac3e34d8b43a: (a: number, b: number, c: any) => void;
-export const wasm_bindgen__convert__closures_____invoke__h37b602cc3e426610: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__hf35e2ecc029564cf: (a: number, b: number) => void;
 export const __wbindgen_malloc: (a: number, b: number) => number;
 export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
 export const __externref_table_alloc: () => number;
