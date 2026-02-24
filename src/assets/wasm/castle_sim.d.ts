d69deb9b6ec31e49ba2df2188e835778737ae357
PodeCaradox <gamer3911@web.de>
2026-02-24 18:51:50 +0100

    update versions

--- src/assets/wasm/castle_sim.d.ts
+++ src/assets/wasm/castle_sim.d.ts
@@ -8,18 +8,28 @@ export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembl
 export interface InitOutput {
     readonly memory: WebAssembly.Memory;
     readonly init_game: (a: number, b: number) => void;
-    readonly wasm_bindgen__closure__destroy__h269b17735e5fc350: (a: number, b: number) => void;
-    readonly wasm_bindgen__closure__destroy__hd9e334d357d3bb07: (a: number, b: number) => void;
-    readonly wasm_bindgen__convert__closures_____invoke__h6dd67367705a18ed: (a: number, b: number, c: any, d: any) => void;
-    readonly wasm_bindgen__convert__closures_____invoke__h1032700cb4796663: (a: number, b: number, c: any) => void;
-    readonly wasm_bindgen__convert__closures_____invoke__h3ac7ac3e34d8b43a: (a: number, b: number, c: any) => void;
-    readonly wasm_bindgen__convert__closures_____invoke__hf35e2ecc029564cf: (a: number, b: number) => void;
+    readonly wasm_bindgen__closure__destroy__h171e54c78a02aa44: (a: number, b: number) => void;
+    readonly wasm_bindgen__closure__destroy__h201731ff488da402: (a: number, b: number) => void;
+    readonly wasm_bindgen__closure__destroy__h2267c5a3a33d6ca1: (a: number, b: number) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__hcb093511f31953b8: (a: number, b: number, c: any, d: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h72401b5efe02f859: (a: number, b: number, c: any) => [number, number];
+    readonly wasm_bindgen__convert__closures_____invoke__h36a805dfca600d37: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h03b69bc391c244c3: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h03b69bc391c244c3_3: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h03b69bc391c244c3_4: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h03b69bc391c244c3_5: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h03b69bc391c244c3_6: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h03b69bc391c244c3_7: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h03b69bc391c244c3_8: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h03b69bc391c244c3_9: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h0a4e10f01bf099b6: (a: number, b: number) => void;
     readonly __wbindgen_malloc: (a: number, b: number) => number;
     readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
     readonly __externref_table_alloc: () => number;
     readonly __wbindgen_externrefs: WebAssembly.Table;
     readonly __wbindgen_exn_store: (a: number) => void;
     readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __externref_table_dealloc: (a: number) => void;
     readonly __wbindgen_start: () => void;
 }
 
